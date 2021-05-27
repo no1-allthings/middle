b0f36abaaa5424cb448cd4b54126d19ee16743fc
no1-allthings <no1.allthings@gmail.com>
2021-05-27 19:47:31 +0900

    m-4

--- middle/class.py
+++ middle/class.py
@@ -53,29 +53,98 @@
 # print(s.get_s())
 # print(s.eng) # 직접 변수에 접근뫃하게 했음
 
-class Ppp:
-    def __init__(self):
-        self.name = '란미'
-        self.age = 45
+# class Ppp:
+#     def __init__(self):
+#         self.name = '란미'
+#         self.age = 45
+#
+# class Kor(Ppp):
+#     def say(self):
+#         print('안녕')
+#
+# class Ame(Ppp):
+#     def __init__(self):
+#         super().__init__()
+#         self.lang = '영어'
+#     def say(self):
+#         print('hello')
+#
+# pp = Ppp()
+# kk = Kor()
+# aa = Ame()
+#
+# print(pp.name)
+# print(aa.age)
+# print(kk.name, aa.age, )
+#
+# kk.say()
+# aa.say()
 
-class Kor(Ppp):
-    def say(self):
-        print('안녕')
+# class P:
+#     def __init__(self):
+#         self.x = int(input('x:'))
+#         self.y = int(input('y: '))
+#     def dis(self):
+#         pass
+#
+# class R(P):
+#     def __init__(self):
+#         super().__init__()
+#         self.w = int(input('w: '))
+#         self.h = int(input('h: '))
+#     def dis(self):
+#         print('사각',self.x,self.y,self.w,self.h)
+#
+# class C(P):
+#     def __init__(self):
+#         super().__init__()
+#         self.r = int(input('r: '))
+#     def dis(self):
+#         print('원형', self.x ,self.y, self.r)
+#
+# def main():
+#     l=[]
+#     while True:
+#         s = input('1.사각형, 2.원형, 3.조회, 4.졸료')
+#         if s == '1':
+#             l.append(R())
+#         if s == '2':
+#             l.append(C())
+#         if s == '3':
+#             for i in l:
+#                 i.dis()
+#         if s == '4':
+#             break
+#     print('프로그램 종료')
+# main()
 
-class Ame(Ppp):
-    def __init__(self):
-        super().__init__()
-        self.lang = '영어'
-    def say(self):
-        print('hello')
+# class PP:
+#     def __init__(self, sp):
+#         self.li=[]
+#         self.li.append(sp)
+#     def di(self):
+#         print(self.li)
+# p1=PP('우우')
+# p1.di()
+# p2=PP('콜라')
+# p2.di()
+# p3=PP('커피')
+# p3.di()
+# p1=PP('사이다')
+# p1.di()
 
-pp = Ppp()
-kk = Kor()
-aa = Ame()
+class PP:
+    li=[]
+    def __init__(self, sp):
+        PP.li.append(sp)
+    def di(self):
+        print(PP.li)
+p1=PP('우우')
+p1.di()
+p2=PP('콜라')
+p2.di()
+p3=PP('커피')
+p3.di()
+p1=PP('사이다')
+p1.di()
 
-print(pp.name)
-print(aa.age)
-print(kk.name, aa.age, )
-
-kk.say()
-aa.say()
